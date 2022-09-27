5331933d45407abc1bf89d8cd31bac586308b9cf
Jason Ryberg <jason@ryberg.dev>
2022-09-27 09:32:31 -0700

    tweak backend

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,15 +4,8 @@ terraform {
   }
 
   backend "azurerm" {
-    key                  = "dev-azure-kubernetes-service.tfstate"
-    environment          = "public"
-    resource_group_name  = "tfstate"
-    storage_account_name = "jryberg"
-    container_name       = "tfstate"
-    client_id            = "f1b82532-da8e-47ee-b652-b477517d9534"
-    subscription_id      = "65213276-e312-4beb-9ee5-aa0b5196e748"
-    tenant_id            = "72f988bf-86f1-41af-91ab-2d7cd011db47"
-    use_oidc             = true
+    environment = "public"
+    use_oidc    = true
   }
 }
 
