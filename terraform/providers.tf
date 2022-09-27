c2d53eacad4e2196f19ece205c364030c6048e26
Jason Ryberg <jason@ryberg.dev>
2022-09-27 09:53:43 -0700

    remove hardcoded creds

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -10,11 +10,7 @@ terraform {
 }
 
 provider "azurerm" {
-  client_id            = "f1b82532-da8e-47ee-b652-b477517d9534"
-  subscription_id      = "65213276-e312-4beb-9ee5-aa0b5196e748"
-  tenant_id            = "72f988bf-86f1-41af-91ab-2d7cd011db47"
-  use_oidc             = true
-
+  use_oidc                   = true
   skip_provider_registration = true
-  features {}
+  features { }
 }
