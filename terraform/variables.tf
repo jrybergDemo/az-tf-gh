5679d31f8b17878183a525b788b0bd21c0be039b
Ariel Ramirez <ariel.ramirez@microsoft.com>
2022-10-31 10:36:28 -0700

    adding pe and sa config

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,130 @@ variable "resource_groups" {
     location = string
   }))
 }
+
+variable "resource_groups" {
+  type    = any
+  default = null
+} // Which one should we use? 
+
+variable "vnets" {
+  type    = object({
+    name                = string
+    address_space       = list(string)
+    location            = string
+    resource_group_name = string
+  })
+  default = null
+}
+
+variable "subnets" {
+  type    = any
+  default = null
+}
+
+variable "pip" {
+  type    = object({
+    name                = string
+    location            = string
+    resource_group_name = string
+    allocation_method   = string
+    sku                 = string
+
+  })
+  default = null
+}
+
+variable "bastion" {
+  type    = object({
+    name                = string
+    location            = string
+    resource_group_name = string
+    ip_configuration = object({
+      name        = string
+      subnet_name = string
+    })
+  })
+  default = null
+}
+
+variable "nic" {
+  type    = object({
+    name                = string
+    location            = string
+    resource_group_name = string
+    ip_configuration = object({
+      name        = string
+      subnet_name = string
+      private_ip_address_allocation = string
+    })
+  })
+  default = null
+}
+
+variable "virtual_machine" {
+  type    = object({
+    name                = string
+    location            = string
+    resource_group_name = string
+    size                = string
+    admin_username      = string
+    admin_password      = string
+    os_disk = object({
+      caching              = string
+      storage_account_type = string
+    })
+    source_image_reference = object({
+      publisher = string
+      offer     = string
+      sku       = string
+      version   = string
+    })
+  })
+  default = null
+}
+
+variable "storage_account" {
+  type    = object({
+    name                     = string
+    resource_group_name      = string
+    location                 = string
+    account_tier             = string
+    account_replication_type = string
+  })
+  default = null
+}
+
+variable "private_dns_zone" {
+  type    = object({
+    name                = string
+    resource_group_name = string
+  })
+  default = null
+}
+
+variable "private_dns_zone_vnet_link" {
+  type    = object({
+    name                  = string
+    resource_group_name   = string
+    private_dns_zone_name = string
+  })
+  default = null
+}
+
+variable "private_endpoint" {
+  type    = object({
+    name                = string
+    resource_group_name = string
+    location            = string
+    subnet_name         = string
+    private_service_connection = object({
+      name                 = string
+      is_manual_connection = bool
+      subresource_names    = list(string)
+    })
+    private_dns_zone_group = object({
+      name = string
+    })
+  })
+  default = null
+}
